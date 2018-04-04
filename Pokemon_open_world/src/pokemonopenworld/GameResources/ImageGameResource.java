c57611e394bae09d64eb75d36cd24e95be27887e
Vojtech Brezina <vojta.brezina@gmail.com>
2018-04-04 21:13:33 +0200

    rework of the resourcing system

--- Pokemon_open_world/src/pokemonopenworld/GameResources/ImageGameResource.java
+++ Pokemon_open_world/src/pokemonopenworld/GameResources/ImageGameResource.java
@@ -20,44 +20,20 @@ import javax.imageio.*;
  */
 public class ImageGameResource extends GameResource {
     private Image image;
-    private final boolean fromAtlas;
-    private final AtlasGameResource sourceAtlas;
-    private final int atlasPosX;
-    private final int atlasPosY;
 
     public ImageGameResource(String name, URL path) throws MalformedURLException {
         super(name, path);
         image = null;
-        fromAtlas = false;
-        sourceAtlas = null;
-        atlasPosX = -1;
-        atlasPosY = -1;
     }
-    //Kdo vi, k cemu je tohle dobre...
-    public ImageGameResource(String name, AtlasGameResource source, int atlasPosX, int atlasPosY) throws MalformedURLException {
-        super(name, null);
-        image = null;
-        fromAtlas = true;
-        sourceAtlas = source;
-        this.atlasPosX = atlasPosX;
-        this.atlasPosY = atlasPosY;
-    }
-    
+   
     @Override
     public boolean load(){
-        if(!fromAtlas){
-            try{
-            image = ImageTransparentBackgroundConverter.makeBackgroundTransparent(ImageIO.read(path));
-            }catch(IOException e){
-                //copy from default pack I guess
-            }
-            
-        }else{
-            if(!sourceAtlas.getEmpty()){
-                
-                //when the AtlasGameResource class is ready to use
-            }
+        try{
+        image = ImageTransparentBackgroundConverter.makeBackgroundTransparent(ImageIO.read(path));
+        }catch(IOException e){
+            //copy from default pack I guess
         }
+
        if(getEmpty())
            failCount++;
        else{
@@ -66,6 +42,10 @@ public class ImageGameResource extends GameResource {
        }
            
         return !getEmpty();
+    }
+    
+    public void setImage(Image image){
+        this.image = image;
     }
     
     public Image getImage(){
@@ -80,11 +60,7 @@ public class ImageGameResource extends GameResource {
     @Override
     public String toString(){
         String toReturn = "";
-        toReturn += "image game resource located ";
-        if(fromAtlas)
-            toReturn += "in atlas (" + sourceAtlas.toString() + ")";
-        else
-            toReturn += "on path \"" + path;
+        toReturn += "image game resource located on path \"" + path;
         toReturn += "\" and called \"" + name + "\" and ";
         if(!getEmpty())
             toReturn += "sized " + image.getWidth(null) + "x" + image.getHeight(null);
